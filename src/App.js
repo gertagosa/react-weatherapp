b5a26f9a9afe030307f0df15599994ba65d19ce5
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-02-27 13:51:45 +0000

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="London" />
         <footer>
           This project is coded by Gerta Gosa and is {""}
           <a
