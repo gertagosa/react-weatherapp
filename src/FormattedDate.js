f4c36092cb9ec1e248b8c9072a8993532eb09338
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-03-01 10:08:33 +0000

    Used the correct icon URL

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 export default function FormattedDate(props) {
   let days = [
     "Sunday",
@@ -11,10 +12,11 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
-  let minutes = props.date.getMinutes();
   if (hours < 10) {
     hours = `0${hours}`;
   }
+
+  let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
