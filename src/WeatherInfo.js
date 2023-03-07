897d0aa6a9e15e386fd65acdd1b008b00f6ef42b
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-03-07 11:46:27 +0000

    Refactored the code and fixed warnings

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,7 +5,7 @@ import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="Weatherinfo">
+    <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <ul>
         <li>
@@ -13,22 +13,22 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <div className="clear-fix">
-            <div className="float-left">
+          <div className="d-flex">
+            <div>
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
-            <div className="float-left">
+            <div>
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity:{props.data.humidity}%</li>
-            <li>Wind:{props.data.wind}m/h</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
